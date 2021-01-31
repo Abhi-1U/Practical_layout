96ca8c786049942cbef6fe9afaff71dfac99a0c6
Abhishek Ulayil <PerricoQ@outlook.com>
2021-01-31 21:09:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+namespace teesridimension
+{
+    static class Program
+    {
+        /// <summary>
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form1 app = new Form1();
+            app.Width = 800;
+            app.Height = 600;
+            app.InitializeGraphics();
+            app.Show();
+            while (app.Created)
+            {
+                app.Render();
+                Application.DoEvents();
+            }
+            app.DisposeGraphics();
+        }
+    }
+}
